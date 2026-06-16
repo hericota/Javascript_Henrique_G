1e3fddcb66aa7920d2423b23f8258c9ccfd4f8b3
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-06-16 17:20:01 -0300

    for_while

---
+++ Aula 05/atividades/js/script.js
@@ -0,0 +1,22 @@
+// let soma = 0
+// for(let i = 0; i<=500; i++){
+//     if(i%2==0){
+//         console.log("par:"+i)
+//     }
+//     else{
+//         console.log("impar:"+i)
+//     }
+// }
+
+opcao = true
+while (opcao) {
+    console.log("bom dia")
+    let escolha = prompt("quer parar s/n").toLowerCase()
+    if (escolha == s) {
+        alert("tchau")
+        opcao = false
+    }
+    else {
+        continue
+    }
+}
